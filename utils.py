42028cf5929fe05c091687ef7a9fbf88259e1e6c
Rohan Rajput <rohan@ownedoutcomes.com>
2018-05-23 14:54:32 -0700

    Adding docker compose yaml

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def get_env_var_settings(env_var_name, default_value):
     try:
         env_var_value = os.environ[env_var_name]
 
-    except EnvironmentError:
+    except :
         env_var_value = default_value
 
     return env_var_value
